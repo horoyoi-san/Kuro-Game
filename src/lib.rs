a7ba0509ada750d8e9951621889df35831aab471
Horoyoi-san <korrawitcompara@gmail.com>
2024-12-12 00:07:54 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use windows::Win32::{Foundation::HINSTANCE, System::LibraryLoader::GetModuleHand
 
 mod interceptor;
 
-const FPAKFILE_CHECK: usize = 0x403AD00;
+const FPAKFILE_CHECK: usize = 0x3FE1560;
 
 unsafe fn thread_func() {
     Console::AllocConsole().unwrap();
